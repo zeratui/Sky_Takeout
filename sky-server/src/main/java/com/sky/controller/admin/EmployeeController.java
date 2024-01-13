9179be9636772c3639f4dca1ca554d16410f0ddf
zeratul <935184280@qq.com>
2024-01-13 15:18:35 +0800

    启用禁用员工账号

--- sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
+++ sky-server/src/main/java/com/sky/controller/admin/EmployeeController.java
@@ -100,4 +100,13 @@ public class EmployeeController {
         PageResult pageResult = employeeService.pageQuery(employeePageQueryDTO);
         return Result.success(pageResult);
     }
+
+    @PostMapping("/status/{status}")
+    @ApiOperation("启用禁用员工账号")
+    public Result startOrStop(@PathVariable Integer status,Long id){
+        log.info("启用禁用员工账号：状态{},id{}",status,id);
+        employeeService.startOrStop(status, id);
+
+        return Result.success();
+    }
 }
